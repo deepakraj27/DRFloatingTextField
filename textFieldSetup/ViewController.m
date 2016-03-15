5e33e1e0d59879dc7856a8e333dcf74aabfff6f1
deepakraj murugesan <deepakraj.murugesan@gmail.com>
2016-03-15 19:26:36 +0530

    check commit

--- textFieldSetup/ViewController.m
+++ textFieldSetup/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet DRFloatTextField *phoneNumber;
+@property (weak, nonatomic) IBOutlet DRFloatTextField *password;
 
 @end
 
@@ -16,6 +18,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self settingUpTextField];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,4 +27,39 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)settingUpTextField{
+    self.phoneNumber.enableMaterialPlaceHolder = YES;
+    self.phoneNumber.placeholder = @"Phone Number";
+    self.phoneNumber.errorColor = [UIColor colorWithRed:0.910 green:0.329 blue:0.271 alpha:1.000];
+    self.phoneNumber.lineColor = [UIColor grayColor];
+    self.phoneNumber.delegate=self;
+    self.phoneNumber.tag=1;
+    
+    
+    self.password.enableMaterialPlaceHolder = YES;
+    self.password.placeholder = @"Password";
+    self.password.errorColor = [UIColor colorWithRed:0.910 green:0.329 blue:0.271 alpha:1.000];
+    self.password.lineColor = [UIColor grayColor];
+    self.password.delegate=self;
+    self.password.tag=2;
+
+}
+
+
+-(void)textFieldDidEndEditing:(DRFloatTextField *)textField{
+    if (textField.text.length==0) {
+        [textField showError];
+    }else{
+        [textField hideError];
+    }
+}
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    UIView *view = [self.view viewWithTag:textField.tag + 1];
+    if (!view)
+        [textField resignFirstResponder];
+    else
+        [view becomeFirstResponder];
+    return YES;
+    
+}
 @end
